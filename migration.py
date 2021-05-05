cddeeafd84427907968eba8542c80e1a264c0a7e
Harsha <harsha.prd@gmail.com>
2021-05-05 15:59:59 +0530

    Create migration.py

---
+++ migration.py
@@ -0,0 +1,12 @@
+"""
+1. mysql workbench
+or
+1. using the pgsql schema from the backup, create tables in mysql, changing up the datatypes and syntax accordingly (courtesy - https://dev.mysql.com/doc/workbench/en/wb-migration-database-postgresql-typemapping.html ) 
+     a. import the pgsqldump into the mysql instance
+     or
+     a. connect to both instances of db, select from each table in pgsql and insert into mysql
+or
+1. pg2mysql client apparently could work since there isnt anything out of the box - views, indexes in particular
+
+the question will be answered this week on monday night RAW where John Cena defends his title taking on Sheamus. and there's no punchline thasall
+"""
